f7301af6ee7b5008b4130ac8ed843bb9f2f349b1
Aleksey <alekseytim@mil.ru>
2025-03-31 16:57:11 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: config.corsOrigin,
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
