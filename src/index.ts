248952b597d3cf89fbe21529f5ed32bfb0534a13
Aleksey <alekseytim@mil.ru>
2025-03-31 21:30:29 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.options('*', cors());
 
 // Добавляем заголовки вручную (если нужно)
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://userss.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://usersfront.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
