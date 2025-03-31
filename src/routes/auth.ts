e6ae2e125f3186b8cc0205c39a76fb0d51b186eb
Aleksey <alekseytim@mil.ru>
2025-03-31 16:31:14 +0200

    fr try 2

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -32,7 +32,7 @@ router.post('/register', (async (req, res) => {
 
     console.log('Registration attempt for:', email);
 
-    // Проверяем, существует ли пользователь1
+    // Проверяем, существует ли пользователь12
     const existingUser = await userRepository.findOne({ where: { email } });
     if (existingUser) {
       console.log('User already exists:', email);
