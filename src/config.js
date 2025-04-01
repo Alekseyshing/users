83c7ceb97780d311300a89445d43b29a5e4317a9
Aleksey <alekseytim@mil.ru>
2025-04-01 10:08:19 +0200

    new cors url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   port: process.env.PORT || 5001,
-  corsOrigin: process.env.CORS_ORIGIN || 'https://userss.vercel.app',
-  apiUrl: process.env.API_URL || 'https://userss.vercel.app'
+  corsOrigin: process.env.CORS_ORIGIN || 'https://usersfront.vercel.app',
+  apiUrl: process.env.API_URL || 'https://usersfront.vercel.app'
 };
 
 export default config; 
--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ interface Config {
 
 const config: Config = {
   port: Number(process.env.PORT) || 5001,
-  corsOrigin: process.env.CORS_ORIGIN || 'https://userss.vercel.app',
-  apiUrl: process.env.API_URL || 'https://userss.vercel.app'
+  corsOrigin: process.env.CORS_ORIGIN || 'https://usersfront.vercel.app',
+  apiUrl: process.env.API_URL || 'https://usersfront.vercel.app'
 };
 
 export default config; 
